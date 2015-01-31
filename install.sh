43da59bc0ca070d7e92970a0a6b684278a884c01
Vinicius Souza <vsouza@motorola.com>
2015-01-31 17:11:23 -0300

    Fixing the install script

--- install.sh
+++ install.sh
@@ -1,4 +1,18 @@
-sudo apt-get install python2.7 python-setuptools python-dev libxml2-dev libxslt-dev zlib1g-dev
-sudo easy_install pip
+sudo apt-get install python2.7 python-dev libxml2-dev libxslt-dev zlib1g-dev
 
-pip install -r requirements.txt
+# the user is inside a virtual environment
+if [ -n "$VIRTUAL_ENV" ]; then
+
+	echo "Using a virtual environment: just run pip install"
+
+	pip install -r requirements.txt
+
+else
+	echo "Not using a virtual environment; install setuptools and pip system-wide"
+
+	sudo apt-get install python-setuptools
+
+	sudo easy_install pip
+
+	sudo pip install -r requirements.txt
+fi
